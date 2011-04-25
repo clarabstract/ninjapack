0d29f0135597d9179f3bc35a58dbd2d151bfc3ef
Ruy Asan <ruy@demonware.net>
2011-04-25 09:44:25 -0700

    Add install script, nano settings, etc

---
+++ zsh-init.zsh
@@ -0,0 +1,9 @@
+export NINJA_HOME = $(dirname $0)
+export NINJA_ZSH="NINJA_HOME/zsh"
+
+source $NINJA_ZSH/osx.zsh
+source $NINJA_ZSH/keybindings.zsh
+source $NINJA_ZSH/prompt.zsh
+source $NINJA_ZSH/completions.zsh
+source $NINJA_ZSH/colors.zsh
+
--- zsh/editor.zsh
+++ zsh/editor.zsh
@@ -1,1 +1,7 @@
-export EDITOR=/usr/bin/mate
+if [[ -e /usr/bin/mate ]]; then
+	export EDITOR=/usr/bin/mate
+elif [[ -e /usr/bin/nano ]]; then
+	export EDITOR=/usr/bin/nano
+else
+	echo "Neither TextMate nor nano present, enjoy your VI."
+fi
