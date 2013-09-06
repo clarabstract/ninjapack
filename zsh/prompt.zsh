d1757f9cc6ace3187c1b5342f309b67e43959a86
Ruy Asan <ruyasan@gmail.com>
2013-09-06 12:12:57 -0700

    give up on the unicode prompt arrow
    I don't know whose blood to sacrifice to get PuTTY to display it. Let's just forget about it.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,5 +2,5 @@ BASE_COLOR='yellow'
 ROOT_COLOR='red'
 
 
-export PROMPT="%F{$BASE_COLOR}%~%(!.%F{$ROOT_COLOR}.%F{$BASE_COLOR}) ➤  %f"
+export PROMPT="%F{$BASE_COLOR}%~%(!.%F{$ROOT_COLOR}.%F{$BASE_COLOR}) > %f"
 export RPROMPT="%F{$BASE_COLOR}%m%f"
